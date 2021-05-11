9bf4f8934ea3bf7402778cbef19b994e7723df86
isaias-santos-dev <zackblackmangold>
2021-05-11 14:31:21 -0300

    troca de formato

---
+++ listas_ligadas.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+// criar struct no
+typedef struct no No;
+struct no
+{
+    int num;// valor
+    struct no *proximo;//ponteiro da struct no
+};
+
+int main()
+{
+    
+
+    return 0;
+}
